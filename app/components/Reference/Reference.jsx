64952821518c39a7078abb9a18ea12c4e3c4a2c7
svoby <svobymates@gmail.com>
2024-02-28 17:56:40 +0100

    reference

--- app/components/Reference/Reference.jsx
+++ app/components/Reference/Reference.jsx
@@ -1,7 +1,49 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./Reference.module.css";
 import Image from "next/image";
 
+const clients = [
+  {
+    name: "Revenge",
+    instagram: "1,327,069",
+    youtube: "1,327,069",
+    image: "revenge.png",
+  },
+  {
+    name: "Jarvis",
+    tiktok: "3M+",
+    instagram: "2M+",
+    youtube: "5M+",
+    image: "jarvis.png",
+  },
+  {
+    name: "MrSavage",
+    tiktok: "3M+",
+    instagram: "2M+",
+    youtube: "2M+",
+    image: "savage.png",
+  },
+  {
+    name: "Faze Replays",
+    tiktok: "750K+",
+    instagram: "500K+",
+    youtube: "1.3M+",
+    image: "replays.png",
+  },
+  {
+    name: "Jon Sandman",
+    tiktok: "550K+",
+    instagram: "100K+",
+    youtube: "1M+",
+    image: "jon.png",
+  },
+];
+
 const Reference = () => {
+  const [client, setClient] = useState(0);
+
   return (
     <section className={styles.reference} id="references">
       <h3>
@@ -12,63 +54,77 @@ const Reference = () => {
         <div className={styles.reference_main}>
           <div className={styles.channel}>
             <Image
-              src="/channels/revenge.png"
+              src={`/channels/${clients[client].image}`}
               width={91}
               height={91}
               alt="Picture of the author"
             />
             <div className={styles.channel_info}>
-              <h4>Revenge</h4>
+              <h4>{clients[client].name}</h4>
               <p>
-                <b>1,327,069 </b>Instagram Followers
+                <b>{clients[client].instagram} </b>Instagram Followers
               </p>
               <p>
-                <b>1,327,069 </b> Youtube Subscribers
+                <b>{clients[client].youtube}</b> Youtube Subscribers
               </p>
               <span>Dupscaled since March of 2023.</span>
             </div>
           </div>
 
-          <Image
-            src="/clients.svg"
-            width={53}
-            height={53}
-            alt="Arrow for clients"
-          />
+          <div
+            className={styles.next}
+            onClick={() => {
+              if(client + 1 === clients.length )
+              {
+                return  setClient(0);
+              }
+              setClient(client + 1);
+            }}
+          >
+            <Image
+              className={styles.next}
+              src="/clients.svg"
+              width={53}
+              height={53}
+              alt="Arrow for clients"
+            />
+          </div>
 
           <div className={`${styles.channel} ${styles.channel_opacity}`}>
             <Image
-              src="/channels/revenge.png"
+              src={`/channels/${clients[client].image}`}
               width={91}
               height={91}
               alt="Picture of the author"
             />
             <div className={styles.channel_info}>
-              <h4>Revenge</h4>
+              <h4>{clients[client].name}</h4>
               <p>
-                <b>1,327,069 </b>Instagram Followers
+                <b>{clients[client].instagram} </b>Instagram Followers
               </p>
               <p>
-                <b>1,327,069 </b> Youtube Subscribers
+                <b>{clients[client].youtube}</b> Youtube Subscribers
               </p>
               <span>Dupscaled since March of 2023.</span>
             </div>
           </div>
 
-          <div className={`${styles.channel} ${styles.channel_opacity}`}>
+          <div
+            className={`${styles.channel} ${styles.channel_opacityfix} ${styles.channel_opacity} `}
+          >
             <Image
-              src="/channels/revenge.png"
+              src={`/channels/${clients[client].image}`}
               width={91}
               height={91}
               alt="Picture of the author"
             />
             <div className={styles.channel_info}>
-              <h4>Revenge</h4>
+              <h4>{clients[client].name}</h4>
               <p>
-                <b>1,327,069 </b>Instagram Followers
+                <b>{clients[client].instagram} </b>Instagram Followers
               </p>
               <p>
-                <b>1,327,069 </b> Youtube Subscribers
+                <b>{clients[client].youtube}</b> Youtube Subscribers
               </p>
               <span>Dupscaled since March of 2023.</span>
             </div>
