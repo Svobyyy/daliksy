ddded2896a7a7d85a1c25a9a338506a6921f07c1
svoby <svobymates@gmail.com>
2024-02-29 04:22:18 +0100

    animations

--- app/components/Landing/Inline.jsx
+++ app/components/Landing/Inline.jsx
@@ -16,10 +16,6 @@ const Inline = () => {
     rootElement && (
       <PopupWidget
         url="https://calendly.com/dupscaled/free-meeting"
-        /*
-         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-         */
         rootElement={rootElement}
         text="Click here to schedule!"
         textColor="#ffffff"
