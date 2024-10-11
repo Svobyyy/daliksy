15f04ae2272742c89e7e37433bfc41b0dd5ca7ea
svoby <svobymates@gmail.com>
2024-10-11 13:44:26 +0200

    References Added

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,13 @@ import Footer from "@/components/Footer/Footer";
 import Form from "@/components/Form/Form";
 import Landing from "@/components/Landing/Landing";
 import Clients from "@/components/Clients/Clients";
+import References from "@/components/References/References";
 
 export default function Home() {
   return (
     <main>
       <Landing language="en" />
-      <References language={language} />
+      <References language="en" />
       <Clients language="en" />
       <Categories language="en" />
       <Form language="en" />
